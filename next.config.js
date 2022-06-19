4e3916aba0af8514a068616c58cc0eb0f5db1ff2
KIMSANGYEOB <sykim10070@gmail.com>
2022-06-19 23:57:59 +0900

    google oauth 기능 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['asnrqstutzbsnalgsmsr.supabase.in'],
+    domains: ['asnrqstutzbsnalgsmsr.supabase.in', 'lh3.googleusercontent.com'],
   },
 };
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,86 @@
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
+import nodemailer from 'nodemailer';
+// pages/api/[...nextauth].js
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+// Instantiate Prisma Client
+const prisma = new PrismaClient();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+  secure: true,
+});
+
+const sendWelcomeEmail = async ({ user }) => {
+  const { email } = user;
+
+  try {
+    const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+      encoding: 'utf8',
+    });
+    const emailTemplate = Handlebars.compile(emailFile);
+    await transporter.sendMail({
+      from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+      to: email,
+      subject: 'Welcome to SupaVacation! 🎉',
+      html: emailTemplate({
+        base_url: process.env.NEXTAUTH_URL,
+        support_email: 'support@themodern.dev',
+      }),
+    });
+  } catch (error) {
+    console.log(`❌ Unable to send welcome email to user (${email})`);
+  }
+};
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+const sendVerificationRequest = ({ identifier, url }) => {
+  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+    encoding: 'utf8',
+  });
+  const emailTemplate = Handlebars.compile(emailFile);
+  transporter.sendMail({
+    from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for SupaVacation',
+    html: emailTemplate({
+      base_url: process.env.NEXTAUTH_URL,
+      signin_url: url,
+      email: identifier,
+    }),
+  });
+};
+
+// pages/api/auth/[...nextauth].js
+export default NextAuth({
+  pages: {
+    signIn: '/',
+    signOut: '/',
+    error: '/',
+    verifyRequest: '/',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      maxAge: 10 * 60, // Magic links are valid for 10 min only
+      sendVerificationRequest,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  events: { createUser: sendWelcomeEmail },
+});
